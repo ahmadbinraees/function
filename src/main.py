e5a7ad30ab3fd938dc51bc70a23f7e0a58efd498
root <root@localhost>
2022-04-27 16:18:11 +0600

    auto publish 1651054691163

---
+++ src/main.py
@@ -0,0 +1,16 @@
+# a simple function
+
+def fan_on():
+  print("Get up")
+  print("Go to switch board")
+  print("Turn On the fan")
+
+fan_on()
+#this is a simple funtion what i wrote on top
+
+
+
+#now i'm writing xtra code in function
+
+def ():
+
